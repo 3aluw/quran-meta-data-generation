42d8cc77b96e78724f3ed8864bdb81267ec56a93
Abdou <2000abdallah2014@gmail.com>
2025-08-14 20:15:13 +0100

    implement levenshtein algorithm to compare strings + use new Qaloun dataset

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var quran_meta_1 = require("quran-meta");
-var quran_1 = require("./quran");
+var QalounData_1 = require("./QalounData");
+var thumuns_1 = require("./thumuns");
 function normalizeArabic(text) {
     return text
         // Replace special letters (ٱ → ا)
@@ -14,49 +14,72 @@ function normalizeArabic(text) {
         .replace(/\u06CC/g, 'ي') // Persian ی → Arabic ي (optional)    
         .replace(/\s+/g, ''); // Remove all whitespace
 }
+// Levenshtein distance
+function levenshtein(a, b) {
+    var matrix = Array.from({ length: a.length + 1 }, function () { return []; });
+    for (var i = 0; i <= a.length; i++)
+        matrix[i][0] = i;
+    for (var j = 0; j <= b.length; j++)
+        matrix[0][j] = j;
+    for (var i = 1; i <= a.length; i++) {
+        for (var j = 1; j <= b.length; j++) {
+            var cost = a[i - 1] === b[j - 1] ? 0 : 1;
+            matrix[i][j] = Math.min(matrix[i - 1][j] + 1, // deletion
+            matrix[i][j - 1] + 1, // insertion
+            matrix[i - 1][j - 1] + cost // substitution
+            );
+        }
+    }
+    return matrix[a.length][b.length];
+}
 function getFirstLetters(text, count) {
-    if (count === void 0) { count = 5; }
+    if (count === void 0) { count = 8; }
     var normalized = normalizeArabic(text);
     // Only keep Arabic letters (ignoring digits, punctuation, etc.)
     var lettersOnly = normalized.match(/[\u0621-\u063A\u0641-\u064A]/g) || [];
     return lettersOnly.slice(0, count).join('');
 }
 function compareFirstLetters(text1, text2) {
-    return getFirstLetters(text1) === getFirstLetters(text2);
+    //return getFirstLetters(text1) === getFirstLetters(text2);
+    return levenshtein(getFirstLetters(text1), getFirstLetters(text2)) <= 2;
 }
 var isAyahTextRight = function (text, ayahId) {
-    var ayahLocation = (0, quran_meta_1.findSurahAyahByAyahId)(ayahId).reduce(function (acc, cur, index) {
-        index === 0 ? acc.surahNo = cur : acc.ayahNo = cur;
-        return acc;
-    }, { surahNo: 0, ayahNo: 0 });
-    var foundAyahText = quran_1.quranJson[ayahLocation.surahNo - 1].verses[ayahLocation.ayahNo - 1].text;
+    var _a;
+    var foundAyahText = (_a = QalounData_1.quranData[ayahId - 1]) === null || _a === void 0 ? void 0 : _a.aya_text;
     return compareFirstLetters(text, foundAyahText);
 };
 var checkSurroundingAyahs = function (text, ayahId) {
-    console.log("ayahId", ayahId);
-    var searchAyahId = ayahId - 30;
-    var endAyahId = ayahId + 30;
-    console.log('searchAyahId', searchAyahId);
-    //make sure that ayahId is valid 1=< ayaId <= 6236
-    searchAyahId = searchAyahId >= 1 ? searchAyahId : 1;
-    endAyahId = endAyahId <= 6236 ? endAyahId : 6236;
-    console.log('searchAyahId', searchAyahId);
-    while (searchAyahId <= endAyahId) {
-        if (isAyahTextRight(text, searchAyahId))
-            return searchAyahId;
-        searchAyahId++;
+    var maxId = 6236;
+    var maxOffset = 25;
+    for (var offset = 1; offset <= maxOffset; offset++) {
+        var backId = ayahId - offset;
+        if (backId >= 1 && isAyahTextRight(text, backId)) {
+            return backId;
+        }
+        var forwardId = ayahId + offset;
+        if (forwardId <= maxId && isAyahTextRight(text, forwardId)) {
+            return forwardId;
+        }
     }
     return undefined;
 };
+var findAyah = function (ayahId) { var _a; return (_a = QalounData_1.quranData[ayahId - 1]) === null || _a === void 0 ? void 0 : _a.aya_text; };
+var unfoundAyahs = [];
+var newAyahIds = thumuns_1.HizbEighthList.map(function (verseId, index) {
+    var comparisonText = thumuns_1.textArray[index];
+    var IdAyah = findAyah(verseId);
+    if (!comparisonText)
+        return;
+    var result = isAyahTextRight(comparisonText, verseId) ? verseId : checkSurroundingAyahs(comparisonText, verseId);
+    if (!result)
+        unfoundAyahs.push([verseId, comparisonText, IdAyah]);
+    return result;
+});
 /*
-const unfoundAyahs: [number, string][] = []
-const newAyahIds = verseIds.map((verseId, index) => {
-  const comparisonText = textArray[index]
-  const result = isAyahTextRight(comparisonText, verseId) ? verseId : checkSurroundingAyahs(comparisonText, verseId)
-  if (!result) unfoundAyahs.push([verseId, comparisonText])
-  return result
-})
- */
-/* console.dir(newAyahIds, { depth: null, maxArrayLength: null });
-console.log(unfoundAyahs, { depth: null, maxArrayLength: null }); */
-checkSurroundingAyahs("قال فما خطبك يا سامري", 2399);
+const foundTexts = HizbEighthList.map((verseId, index) => {
+  const obj = { datasetText: textArray[index], foundText: findAyah(verseId) }
+  if (index < 10) console.log(index, obj)
+  return obj
+}) */
+//unfoundAyahs.forEach(e => console.log(e))
+newAyahIds.forEach(function (e) { return console.log(e); });
--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@ import { meta, findPage, getSurahMeta, findSurahAyahByAyahId, checkValidSurahAya
 import type { Surah, Page, AyahId, Juz, AyahNo, SurahMeta } from 'quran-meta'
 
 import { quranJson } from './quran'
-import { thumunObjects, verseIds, textArray } from './thumuns'
+import { quranData } from "./QalounData"
+import { thumunObjects, HizbEighthList, textArray } from './thumuns'
 
 function normalizeArabic(text: string) {
   return text
@@ -17,8 +18,27 @@ function normalizeArabic(text: string) {
     .replace(/\u06CC/g, 'ي') // Persian ی → Arabic ي (optional)    
     .replace(/\s+/g, ''); // Remove all whitespace
 }
+// Levenshtein distance
+function levenshtein(a: String, b: string) {
+  const matrix: number[][] = Array.from({ length: a.length + 1 }, () => []);
 
-function getFirstLetters(text: string, count = 5) {
+  for (let i = 0; i <= a.length; i++) matrix[i][0] = i;
+  for (let j = 0; j <= b.length; j++) matrix[0][j] = j;
+
+  for (let i = 1; i <= a.length; i++) {
+    for (let j = 1; j <= b.length; j++) {
+      const cost = a[i - 1] === b[j - 1] ? 0 : 1;
+      matrix[i][j] = Math.min(
+        matrix[i - 1][j] + 1,    // deletion
+        matrix[i][j - 1] + 1,    // insertion
+        matrix[i - 1][j - 1] + cost // substitution
+      );
+    }
+  }
+  return matrix[a.length][b.length];
+}
+
+function getFirstLetters(text: string, count = 8) {
   const normalized = normalizeArabic(text);
   // Only keep Arabic letters (ignoring digits, punctuation, etc.)
   const lettersOnly = normalized.match(/[\u0621-\u063A\u0641-\u064A]/g) || [];
@@ -26,38 +46,53 @@ function getFirstLetters(text: string, count = 5) {
 }
 
 function compareFirstLetters(text1: string, text2: string) {
-  return getFirstLetters(text1) === getFirstLetters(text2);
+  //return getFirstLetters(text1) === getFirstLetters(text2);
+  return levenshtein(getFirstLetters(text1), getFirstLetters(text2)) <= 2;
 }
 
 const isAyahTextRight = (text: string, ayahId: AyahId) => {
 
-  const ayahLocation = findSurahAyahByAyahId(ayahId).reduce((acc, cur, index) => {
-    index === 0 ? acc.surahNo = cur : acc.ayahNo = cur;
-    return acc;
-  }, { surahNo: 0, ayahNo: 0 });
-
-  const foundAyahText = quranJson[ayahLocation.surahNo - 1].verses[ayahLocation.ayahNo - 1].text;
+  const foundAyahText = quranData[ayahId - 1]?.aya_text;
   return compareFirstLetters(text, foundAyahText)
 }
 
-const checkSurroundingAyahs = (text: string, ayahId: AyahId) => {
-  let searchAyahId = ayahId - 10
-  let endAyahId = ayahId + 10
-  //make sure that ayahId is valid 1=< ayaId <= 6236
-  searchAyahId = searchAyahId >= 1 ? searchAyahId : 1
-  endAyahId = endAyahId <= 6236 ? endAyahId : 6236
-  while (searchAyahId <= endAyahId) {
-    if (isAyahTextRight(text, searchAyahId)) return searchAyahId
-    searchAyahId++
-  }
-  return undefined
-}
+const checkSurroundingAyahs = (text: string, ayahId: number): number | undefined => {
+  const maxId = 6236;
+  const maxOffset = 25;
 
-const unfoundAyahs: [number, string][] = []
-const newAyahIds = verseIds.map((verseId, index) => {
+  for (let offset = 1; offset <= maxOffset; offset++) {
+    const backId = ayahId - offset;
+    if (backId >= 1 && isAyahTextRight(text, backId)) {
+      return backId;
+    }
+
+    const forwardId = ayahId + offset;
+    if (forwardId <= maxId && isAyahTextRight(text, forwardId)) {
+      return forwardId;
+    }
+  }
+
+  return undefined;
+};
+
+const findAyah = (ayahId: AyahId) => quranData[ayahId - 1]?.aya_text;
+
+const unfoundAyahs: [number, string, string][] = []
+
+const newAyahIds = HizbEighthList.map((verseId, index) => {
   const comparisonText = textArray[index]
+  const IdAyah = findAyah(verseId)
+  if (!comparisonText) return
   const result = isAyahTextRight(comparisonText, verseId) ? verseId : checkSurroundingAyahs(comparisonText, verseId)
-  if (!result) unfoundAyahs.push([verseId, comparisonText])
+  if (!result) unfoundAyahs.push([verseId, comparisonText, IdAyah])
   return result
 })
 
+/* 
+const foundTexts = HizbEighthList.map((verseId, index) => {
+  const obj = { datasetText: textArray[index], foundText: findAyah(verseId) }
+  if (index < 10) console.log(index, obj)
+  return obj
+}) */
+//unfoundAyahs.forEach(e => console.log(e))
+newAyahIds.forEach(e => console.log(e))
